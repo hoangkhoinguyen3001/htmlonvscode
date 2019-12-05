4de263ba1c3867a1ae1b70c6f3f5e7fc58195faa
nguyen <hoangkhoinguyen3001@gmail.com>
2019-12-05 18:27:44 +0700

    add element folder

---
+++ demo3/element/classes_body.php
@@ -0,0 +1,32 @@
+<div id="body">
+		<h2>Classes</h2>
+		<div class="content">
+			<div>
+				<div class="section">
+					<h3>Beginners</h3>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent in neque condimentum, dignissim libero sit amet, blandit felis.
+					</p>
+					<span>MWF : 8AM to 9AM</span>
+					<span>TTHS : 10AM-11AM</span>
+				</div>
+				<div class="section">
+					<h3>Intermediate</h3>
+					<p>
+						Cras dignissim est sed lorem suscipit, ut ultrices dolor tempus. Cras venenatis gravida scelerisque. Aenean sit amet massa dui
+					</p>
+					<span>MWF : 10AM-11AM</span>
+					<span>TTHS : 1PM-2PM</span>
+				</div>
+				<div class="section">
+					<h3>Advanced</h3>
+					<p>
+						Quisque non quam lorem. Nulla eu placerat leo. Suspendisse eros risus, viverra sed fermentum vitae, gravida ac tellus.
+					</p>
+					<span>MWF : 1PM-2PM</span>
+					<span>TTHS : 8AM to 9AM</span>
+				</div>
+			</div>
+			<img src="../asset/images/lady-in-yoga.jpg" alt="lady doing yoga" class="figure">
+		</div>
+	</div>
