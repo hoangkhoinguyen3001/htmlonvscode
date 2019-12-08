62afe1527be9e69988d9cfb696e2e2b6c37f5634
nguyen <hoangkhoinguyen3001@gmail.com>
2019-12-08 20:50:59 +0700

    create(data+table)+insert(simple+multiple)+get last id+select

---
+++ demo5/index.php
@@ -0,0 +1,92 @@
+<?php
+//change my.ini in mysql(wamp) to increase timeout and set shocket in this file to open file
+//max_allowed_packet = 10M
+//[mysqldump]
+//quick
+//max_allowed_packet = 10M
+
+ini_set('mysql.connect_timeout', 300);
+ini_set('default_socket_timeout', 300);
+// Create connection
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "myDB";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password,$dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+// Create database(1)
+/*$sql = "CREATE DATABASE myDB";
+if (mysqli_query($conn, $sql)) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . mysqli_error($conn);
+}*/
+
+// sql to create table(2)
+/*$sql = "CREATE TABLE MyGuests (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(30) NOT NULL,
+    lastname VARCHAR(30) NOT NULL,
+    email VARCHAR(50),
+    reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    )";
+    
+    if (mysqli_query($conn, $sql)) {
+        echo "Table MyGuests created successfully";
+    } else {
+        echo "Error creating table: " . mysqli_error($conn);
+    }*/
+
+// insert data(3)
+/*$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('John', 'Doe', 'john@example.com')";
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}*/
+
+// get last id(4)
+/*$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('John', 'Doe', 'john@example.com')";
+
+if (mysqli_query($conn, $sql)) {
+    $last_id = mysqli_insert_id($conn);
+    echo "New record created successfully. Last inserted ID is: " . $last_id;
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}*/
+
+//insert multiple 
+//$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+//VALUES ('John', 'Doe', 'john@example.com')";
+/*$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('Mary', 'Moe', 'mary@example.com');";
+$sql .= "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('Julie', 'Dooley', 'julie@example.com')";
+
+if (mysqli_multi_query($conn, $sql)) {
+    echo "New records created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}*/
+//select data
+/*$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}*/
+mysqli_close($conn);
+?>
